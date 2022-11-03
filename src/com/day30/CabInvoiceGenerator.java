9ef663347230bb902955b9ab3dececb7e8ef7f10
vaidya0303 <vaidya0304@gmail.com>
2022-11-03 17:37:37 +0530

    Given a user id, the invoice service gets the list of rides

--- src/com/day30/CabInvoiceGenerator.java
+++ src/com/day30/CabInvoiceGenerator.java
@@ -20,8 +20,14 @@ package com.day30;
  *          - Total Number Of Rides
  *          - Total Fare
  *          - Average Fare Per Ride
+ *
+ * Step 4 :- Invoice Service
+ *          - Given a user id ,the invoice service gets the list of rides from the ride repository,
+ *            and return the invoice.
  */
 
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * create a class name as CabInvoiceGenerator
@@ -85,18 +91,70 @@ public class CabInvoiceGenerator {
     }
 
     /**
-     *
-     * @param rides
-     * @return
+     * create a method name as invoiceSummaryCalculation
+     * @param rides total no of rides
+     * @return invoice summaray calculation return total ride
      */
     public InvoiceSummary invoiceSummaryCalculation(Ride[] rides) {
+        //variable
         double totalFare = 0.0;
+        /**
+         * using for each loop for total fare
+         */
         for (Ride ride : rides) {
             totalFare += calculateFare(ride.getDistance(), ride.getTime());
         }
+        /**
+         * return total invoice summary
+         */
         return new InvoiceSummary(rides.length, totalFare);
     }
 
+    /**
+     * create a method name as getInvoice
+     * @param userId given user id
+     * @return tatal rides array
+     */
+    public InvoiceSummary getInvoice(int userId) {
+        /**
+         * crete a map object
+         */
+        Map<Integer, Ride[]> map = new HashMap<>();
+        /**
+         * 1st ride
+         */
+        Ride[] rides1 = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        /**
+         * 2nd ride
+         */
+        Ride[] rides2 = {new Ride(5.0, 10),
+                new Ride(1, 1)
+        };
+
+        /**
+         * 3rd ride
+         */
+        Ride[] rides3 = {new Ride(8.0, 15),
+                new Ride(1, 10)
+        };
+        /**
+         * calling put method from map object
+         */
+        map.put(1, rides1);
+        map.put(2, rides2);
+        map.put(3, rides3);
+
+        for (Map.Entry<Integer, Ride[]> entry : map.entrySet()) {
+            if (userId == entry.getKey()) {
+                System.out.println(entry.getKey());
+                Ride[] ridesArray = entry.getValue();
+                return invoiceSummaryCalculation(ridesArray);
+            }
+        }
+        return null;
+    }
 
 
     /**
@@ -113,6 +171,18 @@ public class CabInvoiceGenerator {
          * and print o/p
          */
         System.out.println(cabInvoiceGenerator.calculateFare(5, 20) + " Rs");
-    }
 
-}
+        /**
+         * rides array = distance,time
+         */
+        Ride[] ridesArray = {
+                new Ride(0.1,1),
+                new Ride(1,1),
+                new Ride(1,10)
+        };
+        /**
+         * calling getInvoice method from cabInvoiceGenerator object
+         */
+        cabInvoiceGenerator.getInvoice(1);
+    }
+    }
--- src/com/day30/InvoiceServiceTest.java
+++ src/com/day30/InvoiceServiceTest.java
@@ -52,4 +52,11 @@ public class InvoiceServiceTest {
         InvoiceSummary expectedInvoices = new InvoiceSummary(2, 30.0);
         Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
     }
+    @Test
+    public void givenUserId_ShouldReturnInvoiceSummary() {
+      CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+        InvoiceSummary invoiceSummary = invoiceService.getInvoice(1);
+        InvoiceSummary expectedInvoices = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
+    }
 }
