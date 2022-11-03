2f8eb7344b5d3a59d31fe8fbe292d629607cd342
vaidya0303 <vaidya0304@gmail.com>
2022-11-03 17:42:13 +0530

    Two categories of rides normal rides and premium rides UC5.

--- src/com/day30/CabInvoiceGenerator.java
+++ src/com/day30/CabInvoiceGenerator.java
@@ -24,6 +24,12 @@ package com.day30;
  * Step 4 :- Invoice Service
  *          - Given a user id ,the invoice service gets the list of rides from the ride repository,
  *            and return the invoice.
+ *
+ * Step 5 :- Premium Rides (Bonus)
+ *          - The cab agency now supports 2 categories of rides :
+ *          - Normal Rides : Rs.10 per km, Rs.1 per minute, Minimum fare of Rs.5
+ *          - Premium Rides : Rs.15 per km, Rs.2 per minute, Minimum fare of Rs.20
+ *
  */
 
 import java.util.HashMap;
@@ -154,6 +160,150 @@ public class CabInvoiceGenerator {
             }
         }
         return null;
+    }
+
+    /**
+     * - Premium Rides (Bonus)
+     * - The cab agency now supports 2 categories of rides :
+     * - Normal Rides : Rs.10 per km, Rs.1 per minute, Minimum fare of Rs.5
+     * - Premium Rides : Rs.15 per km, Rs.2 per minute, Minimum fare of Rs.20
+     *
+     */
+
+    /**
+     * final variables does not change its constant
+     */
+    private static final double MINIMUM_COST_PER_KILOMETER_NORMAL = 10;
+    private static final int COST_PER_TIME_NORMAL = 1;
+    private static final double MINIMUM_FARE_NORMAL = 5;
+    public static final double MINIMUM_COST_PER_KILOMETER_PREMIUM = 15;
+    public static final double COST_PER_TIME_PREMIUM = 2;
+    public static final double MINIMUM_FARE_PREMIUM = 20;
+
+    /**
+     * Create a method name as calculateFare
+     * @param distance
+     * @param time
+     * @param type
+     * @return customer type
+     */
+    public static double calculateFare(double distance, int time, String type) {
+        /**
+         *  if type is equal to another string normal
+         */
+        if (type.equalsIgnoreCase("Normal")) {
+            /**
+             * formula minimum fare normal
+             */
+            double totalFare = distance * MINIMUM_COST_PER_KILOMETER_NORMAL + time * COST_PER_TIME_NORMAL;
+            /**
+             * formula minimum fare premium
+             */
+            return Math.max(totalFare, MINIMUM_FARE_NORMAL);
+            /**
+             *  if type is equal to another string premium
+             */
+        } else if (type.equalsIgnoreCase("Premium")) {
+            /**
+             * formula minimum fare premium
+             */
+            double totalFare = distance * MINIMUM_COST_PER_KILOMETER_PREMIUM + time * COST_PER_TIME_PREMIUM;
+            /**
+             *
+             *  Math.max = the larger of a and b.
+             */
+            return Math.max(totalFare, MINIMUM_FARE_PREMIUM);
+            /**
+             * suppose 1st and 2nd condition is false then else is executed
+             */
+        } else {
+            /**
+             * 2 condition is fails then display this msg
+             */
+            System.out.println("Please Enter Proper Customer Type");
+            return 0.0;
+        }
+    }
+
+    /**
+     * for type - total fare calculate
+     * @param rides
+     * @param type
+     * @return total fare
+     */
+    public static double calculateFare(Ride[] rides, String type) {
+        //variable
+        double totalFare = 0.0;
+        /**
+         * if type is normal
+         */
+        if (type.equalsIgnoreCase("Normal")) {
+            /**
+             * using for each loop calculating fare
+             */
+            for (Ride ride : rides) {
+                totalFare += calculateFare(ride.getDistance(), ride.getTime(), type);
+            }
+            /**
+             * return total fare
+             */
+            return totalFare;
+
+            /**
+             * if 1st condition was false then check 2nd condition type is comparing to another string
+             * premium if they are both are equal then two string are equalIgnore case means its true
+             */
+        } else if (type.equalsIgnoreCase("Premium")) {
+            for (Ride ride : rides) {
+                totalFare += calculateFare(ride.getDistance(), ride.getTime(), type);
+
+            }
+            return totalFare;
+            /**
+             * if upper both condition was fail then else statment execute
+             */
+        } else
+        /**
+         * if 1st 2nd condition was false then display this msg
+         */
+            System.out.println("Please Enter Proper Customer Type");
+        return 0.0;
+    }
+
+    /**
+     * create a method name as calculateTotalFare,this is parameterized method
+     * For type- summary invoice
+     * @param rides total rides
+     * @param type normal ,premium
+     * @return invoice summary
+     */
+    public static InvoiceSummary calculateTotalFare(Ride[] rides, String type) {
+        //variable
+        double totalFare = 0.0;
+        /**
+         *  type is comparing to another string
+         * premium if they are both are equal then two string are equalIgnore case means its true
+         */
+        if (type.equalsIgnoreCase("Normal")) {
+            /**
+             * using for each loop for calculate fare
+             */
+            for (Ride ride : rides) {
+                totalFare += calculateFare(ride.getDistance(), ride.getTime(),type);
+            }
+            /**
+             * if 1st condition was false then check 2nd condition type is comparing to another string
+             * premium if they are both are equal then two string are equalIgnore case means its true
+             */
+        } else if (type.equalsIgnoreCase("Premium")) {
+            for (Ride ride : rides) {
+                totalFare += calculateFare(ride.getDistance(), ride.getTime(),type);
+            }
+        }
+        /**
+         * return invoice summary - total fare
+         */
+        return new InvoiceSummary(rides.length, totalFare);
     }
 
 
--- src/com/day30/InvoiceServiceTest.java
+++ src/com/day30/InvoiceServiceTest.java
@@ -6,6 +6,7 @@ package com.day30;
  */
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -13,35 +14,46 @@ import org.junit.Test;
  */
 public class InvoiceServiceTest {
 
-        @Test
-        public void givenDistanceAndTime_ShouldReturnTotalFare() {
-            CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
-            double distance = 2.0;
-            int time = 5;
-            double totalFare = invoiceService.calculateFare(distance, time);
-            Assert.assertEquals(25, totalFare, 0);
-        }
+CabInvoiceGenerator invoiceService;
 
-        @Test
-        public void givenLessDistanceAndTime_ShouldReturnMinFare() {
-            CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
-            double distance = 0.1;
-            int time = 1;
-            double totalFare = invoiceService.calculateFare(distance, time);
-            Assert.assertEquals(5.0, totalFare, 0);
-        }
+    @Before
+    public void before() {
+        invoiceService = new CabInvoiceGenerator();
+    }
 
-        @Test
-        public void givenMultipleRides_ShouldReturnTotalOfTotalFare() {
-            Ride[] rides = {new Ride(2.0, 5),
-                    new Ride(5.0, 10),
-                    new Ride(0.1, 1),
-                    new Ride(20, 60)
-            };
-            CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
-            double totalFare = invoiceService.calculateFareForMultipleRides(rides);
-            Assert.assertEquals(260, totalFare, 0);
-        }
+    // UC1 test cases
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double totalFare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(25, totalFare, 0);
+    }
+    // UC1 test cases
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double totalFare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(5.0, totalFare, 0);
+    }
+
+    // UC2 test cases
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalOfTotalFare() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(5.0, 10),
+                new Ride(0.1, 1),
+                new Ride(20, 60)
+        };
+        CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
+        double totalFare = invoiceService.calculateFareForMultipleRides(rides);
+        Assert.assertEquals(260, totalFare, 0);
+    }
+
+    // UC3 test cases
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary() {
         Ride[] rides = {new Ride(2.0, 5),
@@ -52,11 +64,90 @@ public class InvoiceServiceTest {
         InvoiceSummary expectedInvoices = new InvoiceSummary(2, 30.0);
         Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
     }
+
+    // UC4 test cases
     @Test
     public void givenUserId_ShouldReturnInvoiceSummary() {
-      CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+        CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
         InvoiceSummary invoiceSummary = invoiceService.getInvoice(1);
         InvoiceSummary expectedInvoices = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
+    }
+
+    //UC5 test cases
+    @Test
+    public void givenDistanceAndTime_WhenNormal_ShouldReturnTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        String type = "normal";
+        double fare =  CabInvoiceGenerator.calculateFare(distance, time, type);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_WhenNormal_ShouldReturnMinFare() {
+        double distance = 0.1;
+        int time = 1;
+        String type = "normal";
+        double fare =  CabInvoiceGenerator.calculateFare(distance, time, type);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_WhenNormal_ShouldReturnTotalFare() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+        String type = "normal";
+        double totalFare =  CabInvoiceGenerator.calculateFare(rides, type);
+        Assert.assertEquals(30.0, totalFare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_WhenPremium_ShouldReturnTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        String type = "premium";
+        double fare =  CabInvoiceGenerator.calculateFare(distance, time, type);
+        Assert.assertEquals(40, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_WhenPremium_ShouldReturnMinFare() {
+        double distance = 0.1;
+        int time = 1;
+        String type = "premium";
+        double fare =  CabInvoiceGenerator.calculateFare(distance, time, type);
+        Assert.assertEquals(20, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_WhenPremium_ShouldReturnTotalFare() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+        String type = "premium";
+        double totalFare =  CabInvoiceGenerator.calculateFare(rides, type);
+        Assert.assertEquals(60, totalFare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_WhenNormal_ShouldReturnInvoiceSummary() {
+        String type = "Normal";
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1),
+        };
+        InvoiceSummary invoiceSummary =  CabInvoiceGenerator.calculateTotalFare(rides, type);
+        InvoiceSummary expectedInvoices = new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
+    }
+
+    @Test
+    public void givenMultipleRides_WhenPremium_ShouldReturnInvoiceSummary() {
+        String type = "Premium";
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1),
+        };
+        InvoiceSummary invoiceSummary =  CabInvoiceGenerator.calculateTotalFare(rides, type);
+        InvoiceSummary expectedInvoices = new InvoiceSummary(2, 60.0);
         Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
     }
 }
